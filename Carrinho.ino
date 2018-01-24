88bcfda728a82152bffa35495f0357846fee8ccd
alanubz <35764354+alanubz@users.noreply.github.com>
2018-01-24 08:36:57 -0200

    Add files via upload

---
+++ Carrinho.ino
@@ -0,0 +1,29 @@
+
+#include <SoftwareSerial.h>
+
+#define RxDigital 6
+#define TxDigital 7
+
+//#define DEBUG_ENABLED  1 (Pesquisar o que é)
+
+SoftwareSerial bluetooth(RxDigital,TxDigital);
+
+
+void setup() {
+
+  Serial.begin(9600); //configurando velocidade da porta serial
+
+  pinMode(RxDigital,INPUT); // Configurando o pino digital 6 (RX)
+
+  pinMode(TxDigital,OUTPUT); // Configurando o pino digital 7 (TX)
+
+  
+
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
